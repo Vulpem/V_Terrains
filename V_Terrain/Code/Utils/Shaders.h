021c142d6dbd781cbfb4648f523aad6bb67de478
Vulpem <davidher96@gmail.com>
2018-02-11 11:38:00 +0100

    Added license && Fixed x64 config

--- V_Terrain/Code/Utils/Shaders.h
+++ V_Terrain/Code/Utils/Shaders.h
@@ -1,3 +1,15 @@
+//  V Terrains
+//  Procedural terrain generation for modern C++
+//  Copyright (C) 2018 David Hernàndez Làzaro
+//  
+//  "V Terrains" is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or any later version.
+//  
+//  "V Terrains" is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+//  
+//  For more details, read "COPYING.txt" and "COPYING.LESSER.txt" included in this project.
+//  You should have received a copy of the GNU General Public License along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #include "Globals.h"
