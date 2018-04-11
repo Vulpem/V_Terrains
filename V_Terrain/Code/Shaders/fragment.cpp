9d8ab8b4ad6f478310c2b0e795be0ddebff126c3
Vulpem <davidher96@gmail.com>
2018-04-11 12:33:58 +0200

    Fixed render without light

--- V_Terrain/Code/Shaders/fragment.cpp
+++ V_Terrain/Code/Shaders/fragment.cpp
@@ -98,50 +98,11 @@ void main()
     {
         col *= max(dot(global_light_direction, norm), ambient_min);
     }
-    else
+    
+	/*if(renderDensity)
     {
         col = ScalarToColor(poliDensity / maxDensity);
-    }
+    }*/
 
     color = vec4(mix(col, fog_color, min((dist*dist) / (fog_distance*fog_distance), 1.f)), 1.f);
 }
-
-		/*
-		//Water
-        if (height <= water_height + water_height * 0.0001f)
-        {
-            col = water_color;
-        }
-        //Steep 2
-        else if (norm.y < 0.1f)
-        {
-            if (height < (max_height - water_height) * 0.62f + water_height)
-            {
-                col = vec3(0.535f, 0.613f, 0.672f);
-            }
-            else
-            {
-                col = vec3(0.335f, 0.413f, 0.472f);
-            }
-        }
-        //Steep
-        else if (norm.y < 0.15f && height >(max_height - water_height) * 0.56f + water_height)
-        {
-            col = vec3(0.435f, 0.513f, 0.572f);
-        }
-        //Sand
-        else if (height <= water_height + 30)
-        {
-            col = vec3(0.949f, 0.823f, 0.662f);
-        }
-        //Snow
-        else if (height > (max_height - water_height) * 0.62f + water_height)
-        {
-            col = vec3(0.937f, 0.981f, 1.f);
-        }
-        //Default
-        else
-        {
-            col = vec3(0.478f, 0.8f, 0.561f);
-        }
-		*/
