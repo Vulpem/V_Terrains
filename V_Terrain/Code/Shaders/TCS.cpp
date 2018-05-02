f19fe988ef01b5af0b7e7492f6259b8e8bb90317
dhernandez <davidher96@gmail.com>
2018-05-02 18:44:36 +0200

    stuff

--- V_Terrain/Code/Shaders/TCS.cpp
+++ V_Terrain/Code/Shaders/TCS.cpp
@@ -2,7 +2,12 @@
 
 layout(vertices = 4) out;
 
+uniform  mat4 view_matrix;
+uniform  mat4 projection_matrix;
+uniform  mat4 model_matrix;
+
 in  vec2 UV[];
+in vec3 pos[];
 
 out  vec2 UVs[];
 
@@ -10,17 +15,30 @@ uniform int tesselationDensity[];
 
 //https://developer.nvidia.com/gameworks-vulkan-and-opengl-samples
 
-void main(void) {
-	if (gl_InvocationID == 0)
-	{
-		gl_TessLevelInner[0] = tesselationDensity[0];
-		gl_TessLevelInner[1] = tesselationDensity[1];
+float AdaptiveTessellation(vec3 p0, vec3 p1)
+{
+    vec3 center = (p0 + p1)*0.5f;
+    vec4 clip2 = view_matrix * vec4(center, 1.0);
+    vec4 clip4 = view_matrix * vec4(0, 0, 0, 1.0);
+    float cameraDist = length(clip2.xyz);
+    float cameraDistFromCenter = length(clip4.xyz);
 
-		gl_TessLevelOuter[0] = tesselationDensity[2];
-		gl_TessLevelOuter[1] = tesselationDensity[3];
-		gl_TessLevelOuter[2] = tesselationDensity[4];
-		gl_TessLevelOuter[3] = tesselationDensity[5];
-	}
+    vec4 clip0 = projection_matrix * view_matrix * model_matrix * vec4(p0, 1.0);
+    vec4 clip1 = projection_matrix * view_matrix * model_matrix * vec4(p1, 1.0);
+    const float d = distance(clip0, clip1);
+    float terrTessTriSize = 100.0f;
+    return (1 + clamp((cameraDistFromCenter * 2 / cameraDist)*(d / terrTessTriSize), 1, 50));
+
+}
+
+void main(void) {
+	
+    gl_TessLevelOuter[0] = AdaptiveTessellation(pos[0], pos[3]);
+    gl_TessLevelOuter[1] = AdaptiveTessellation(pos[0], pos[1]);
+    gl_TessLevelOuter[2] = AdaptiveTessellation(pos[1], pos[2]);
+    gl_TessLevelOuter[3] = AdaptiveTessellation(pos[3], pos[2]);
+    gl_TessLevelInner[0] = gl_TessLevelInner[1] = (gl_TessLevelOuter[0] + gl_TessLevelOuter[1] + gl_TessLevelOuter[2] + gl_TessLevelOuter[3])*0.25f;
+
 	UVs[gl_InvocationID] = UV[gl_InvocationID];
 	gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
 }
--- V_Terrain/Code/Shaders/vertex.cpp
+++ V_Terrain/Code/Shaders/vertex.cpp
@@ -4,6 +4,7 @@ in  vec3 position;
 in  vec2 texCoord;
 
 out  vec2 UV;
+out vec3 pos;
 
 uniform  mat4 view_matrix;
 uniform  mat4 model_matrix;
@@ -14,6 +15,7 @@ uniform  sampler2D heightmap;
 
 void main()
 {
+    pos = position;
     gl_Position = vec4(position, 1);
     UV = texCoord;
 }
