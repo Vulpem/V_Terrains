64ab86da924f1ea48b39f034ca752c026593eb36
Vulpem <davidher96@gmail.com>
2018-05-07 20:18:22 +0200

    Fixed some culling, tesselation issues

--- V_Terrain/Code/Shaders/TCS.cpp
+++ V_Terrain/Code/Shaders/TCS.cpp
@@ -1,7 +1,7 @@
 #version 430 core
 
 float heightScale = 1.f;
-float triSize = 35.f;
+float triSize = 30.f;
 //vec4 frustumPlanes[6];
 vec3 tileSize = vec3(1.f, 1.f, 1.f);
 //float tileBoundingSphereR = length(tileSize/2.f);
@@ -20,30 +20,12 @@ out  vec2 UVs[];
 
 //https://developer.nvidia.com/gameworks-vulkan-and-opengl-samples
 
-// transform from world to screen coordinates
 vec2 worldToScreen(vec3 p)
 {
 	vec4 r = (projection_matrix * view_matrix) * vec4(p, 1.0);   // to clip space
 	r.xy /= r.w;            // project
 	r.xy = r.xy*0.5 + 0.5;  // to NDC
-	r.xy *= viewport.xy;    // to pixels
 	return r.xy;
-}
-
-// test if sphere is entirely contained within frustum planes
-bool sphereInScreen(vec3 pos)
-{
-	vec2 p = worldToScreen(pos);
-	p /= viewport.xy;
-
-	return (p.x > 0.0f && p.x < 1.f && p.y > 0.0f && p.y < 1.f);
-}
-
-// calculate edge tessellation level from two edge vertices in screen space
-float calcEdgeTessellation(vec2 s0, vec2 s1)
-{
-	float d = distance(s0, s1);
-	return clamp(d / triSize, 1, 50);
 }
 
 vec2 eyeToScreen(vec4 p)
@@ -51,11 +33,21 @@ vec2 eyeToScreen(vec4 p)
 	vec4 r = projection_matrix * p;   // to clip space
 	r.xy /= r.w;            // project
 	r.xy = r.xy*0.5 + 0.5;  // to NDC
-	r.xy *= viewport.xy;    // to pixels
 	return r.xy;
 }
 
-// calculate tessellation level by fitting sphere to edge
+bool sphereInScreen(vec3 pos, float allowance)
+{
+    vec2 p = worldToScreen(pos);
+    return (p.x > 0.0f - allowance && p.x < 1.f + allowance && p.y < 1.f + allowance);
+}
+
+float calcEdgeTessellation(vec2 s0, vec2 s1)
+{
+    float d = distance(s0 * viewport, s1 * viewport);
+    return clamp(d / triSize, 1, 61);
+}
+
 float calcEdgeTessellationSphere(vec3 w0, vec3 w1, float diameter)
 {
 	vec3 centre = (w0 + w1) * 0.5;
@@ -66,26 +58,17 @@ float calcEdgeTessellationSphere(vec3 w0, vec3 w1, float diameter)
 	return calcEdgeTessellation(s0, s1);
 }
 
-void main() {
+void main()
+{
 	gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
 	UVs[gl_InvocationID] = UV[gl_InvocationID];
 
+	vec3 spherePos = (pos[0] + pos[1] + pos[2] + pos[3]) *0.25f;
+	float sphereDiam = distance(spherePos, pos[0]) * 1.1f;
+    vec2 diamOnScreen = eyeToScreen(vec4(sphereDiam, 0.f, 0.f, 1.f));
 
-	const float minX = min(min(pos[0].x, pos[1].x), min(pos[2].x, pos[3].x));
-	const float minY = min(min(pos[0].y, pos[1].y), min(pos[2].y, pos[3].y));
-	const float minZ = min(min(pos[0].z, pos[1].z), min(pos[2].z, pos[3].z));
-
-	const float maxX = max(max(pos[0].x, pos[1].x), max(pos[2].x, pos[3].x));
-	const float maxY = max(max(pos[0].y, pos[1].y), max(pos[2].y, pos[3].y));
-	const float maxZ = max(max(pos[0].z, pos[1].z), max(pos[2].z, pos[3].z));
-
-	// bounding sphere for patch
-	vec3 spherePos = vec3(minX + maxX, minY + maxY, minZ + maxZ) *0.5f;
-	float sphereR = max(max(maxX - minX, maxY - minY), maxZ - minZ);
-
-	// test if patch is visible
-	if (!sphereInScreen(spherePos)) {
-		// cull patch
+	if (!sphereInScreen(spherePos, 0.15))//length(diamOnScreen) / 2.f))
+    {
 		gl_TessLevelOuter[0] = -1;
 		gl_TessLevelOuter[1] = -1;
 		gl_TessLevelOuter[2] = -1;
@@ -94,23 +77,14 @@ void main() {
 		gl_TessLevelInner[0] = -1;
 		gl_TessLevelInner[1] = -1;
 	}
-	else {
-		// compute automatic LOD
+	else
+    {
+		gl_TessLevelOuter[0] = calcEdgeTessellationSphere(pos[3], pos[0], sphereDiam);
+		gl_TessLevelOuter[1] = calcEdgeTessellationSphere(pos[0], pos[1], sphereDiam);
+		gl_TessLevelOuter[2] = calcEdgeTessellationSphere(pos[1], pos[2], sphereDiam);
+		gl_TessLevelOuter[3] = calcEdgeTessellationSphere(pos[2], pos[3], sphereDiam);
 
-		// calculate edge tessellation levels
-		// see tessellation diagram in OpenGL 4 specification for vertex order details
-
-		// use screen space size of sphere fit to each edge
-
-		gl_TessLevelOuter[0] = calcEdgeTessellationSphere(pos[3], pos[0], sphereR * 2);
-		gl_TessLevelOuter[1] = calcEdgeTessellationSphere(pos[0], pos[1], sphereR * 2);
-		gl_TessLevelOuter[2] = calcEdgeTessellationSphere(pos[1], pos[2], sphereR * 2);
-		gl_TessLevelOuter[3] = calcEdgeTessellationSphere(pos[2], pos[3], sphereR * 2);
-
-		// calc interior tessellation level - use average of outer levels
 		gl_TessLevelInner[0] = 0.5 * (gl_TessLevelOuter[1] + gl_TessLevelOuter[3]);
 		gl_TessLevelInner[1] = 0.5 * (gl_TessLevelOuter[0] + gl_TessLevelOuter[2]);
-
-		//Out[gl_InvocationID].tessLevelInner = vec2(gl_TessLevelInner[0], gl_TessLevelInner[1]);
 	}
 }
